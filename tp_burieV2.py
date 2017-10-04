bc646bf1c9c2925ca9fa38e3e806cf715ed5c38d
Claire Lozano <claire.lozano@live.fr>
2017-10-04 10:09:40 +0200

    open img + gray + bin

--- tp_burieV2.py
+++ tp_burieV2.py
@@ -15,48 +15,26 @@ from astropy.convolution import convolve
 from scipy import signal as sg
 from scipy import ndimage as ndi
 
-def applicationOfFilter(img):
+def applicationOfFilter(img, folder):
 
 	# ==============================
-	# =========== PART 1 ===========
+	# ========== OPEN IMG ==========
 	# ==============================
 
 	# Open initial image
-	imageName1 = "Image1_gaussien_10.png"
-	img = openImage(folder, imageName1)
-	
+	img = openImage(folder, img)
+	plt.imshow(img)
+	plt.show()
 	# Get image in gray
-	gray_image1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-	# Crop image
-	crop_img1 = gray_image1[970:1200, 870:1900] 
-
-	scipy.misc.imsave('crop1.png', crop_img1)
-
-	# Use median filter on the image
-	im_med = ndimage.median_filter(crop_img4, 3)
-	
-	# Save Image
-	scipy.misc.imsave('med1.png', im_med)
-	
-	# Convolution
-	k = np.array([[1,1,1],[1,1,1],[1,1,1]])
-	k = k / 9.0
-	kernel = Gaussian2DKernel(stddev=1)
-
-	im_convolve1 = scipy_convolve(crop_img1, k, mode='same')
+	gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	# Otsu
-	im_convolve1 = im_convolve1.astype(np.uint8)
-	ret, thresh1 = cv2.threshold(im_convolve1,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-	imgInvert1 = np.invert(thresh1)
-
-	kernel = np.ones((2,2), np.uint8)
-	# img_erosion = cv2.erode(im_med3, kernel, iterations=1)
-	img_dilate = cv2.dilate(im_med3, kernel, iterations=1)
+	gray_image = gray_image.astype(np.uint8)
+	ret, thresh = cv2.threshold(gray_image,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+	imgInvert = np.invert(thresh)
 
 	# Save image
-	scipy.misc.imsave('otsu1.png', imgInvert1)
+	scipy.misc.imsave('otsu.png', imgInvert)
 
 	# plt.hist(img.ravel(),256,[0,256])
 	# plt.show()
@@ -64,26 +42,9 @@ def applicationOfFilter(img):
 	# plt.show()
 
 	# ==============================
-	# =========== PART 2 ===========
+	# ========= TRAITEMENT =========
 	# ==============================
 
-	# Get open initial image
-	imageName1 = "image2c.png"
-	image3 = openImage(folder2, imageName3)
-
-	# Get image in gray
-	grayImage1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-	grayImage1 = grayImage1.astype(np.uint8)
-	
-	ret, thresh1 = cv2.threshold(grayImage1,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-	imgInvert1 = np.invert(thresh1)
-
-	# img_erosion = cv2.erode(imgInvert1, kernel, iterations=2)
-	img_dilate = cv2.dilate(imgInvert1, kernel, iterations=1)
-
-	# # Save image
-	scipy.misc.imsave('otsu1Color.png', imgInvert1)
 
 	return 0
 
@@ -107,5 +68,6 @@ def getHistGreyImage(img):
 
 
 # Line : python tp_burie.py img.jpg
-CBIR_BDD = str(sys.argv[1])
-applicationOfFilter(CBIR_BDD)
+img = str(sys.argv[1])
+folder = str(sys.argv[2])
+applicationOfFilter(img, folder)
